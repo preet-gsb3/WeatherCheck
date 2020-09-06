cf363c6e7c58ddf9075ba9748210f310b0e60513
Gurpreet Singh <preet.gsb3@gmail.com>
2020-09-06 14:52:39 +0530

    Initial Commit

---
+++ GurpreetTest_Weather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GurpreetTest_Weather
+//
+//  Created by Gurpreet Singh on 06/09/20.
+//  Copyright © 2020 Gurpreet Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
